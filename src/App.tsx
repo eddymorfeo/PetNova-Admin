b710a4ab77dc347b90a06d3ec1e997c2717d9979
EdwardTejeda <edward.tejeda1987@gmail.com>
2025-04-26 00:39:06 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button className="btn btn-primary" variant="contained">Hello world</Button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
